1dc44127fa08f7ba4fd39f682d94cf6ce6add52e
YaelAdair11 <yaelgtz11@gmail.com>
2025-10-22 01:09:02 -0600

    arreglo supa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import supabase from 'astro-supabase'; // <-- AÑADE ESTA LÍNEA
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server'
+  output: 'server',
+  integrations: [supabase()] // <-- AÑADE ESTA LÍNEA DENTRO DE []
 });
