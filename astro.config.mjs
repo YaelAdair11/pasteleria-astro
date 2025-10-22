b13fef7fd9e4a37ea0afdf5d262403c04568f687
YaelAdair11 <yaelgtz11@gmail.com>
2025-10-22 09:28:49 -0600

    arreglo de lineas

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
-
 import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   // o export const prerender = false en cada endpoint
   output: 'server',
-
   adapter: netlify()
 });
